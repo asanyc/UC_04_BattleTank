4aea2ec870e884b43419eebac7e7393524ba9574
Asa <asanyc@gmail.com>
2017-05-07 03:07:34 -0400

    Video 153 - Fly-by-wire control.  Adding new tankMovementComponent to abstract track throttle away to stick inputs.  Just adding the class.

---
+++ TankScrap/Source/TankScrap/Private/TankMovementComponent.cpp
@@ -0,0 +1,8 @@
+// All mines.
+
+#include "TankScrap.h"
+#include "TankMovementComponent.h"
+
+
+
+
---
+++ TankScrap/Source/TankScrap/Public/TankMovementComponent.h
@@ -0,0 +1,19 @@
+// All mines.
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSCRAP_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
